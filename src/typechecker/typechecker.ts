00ff9efab7dd7386af7708b4b06349ba70ce157f
Wen Hao <52305097+wenhaogoh@users.noreply.github.com>
2023-03-26 13:13:41 +0800

    feat: support binary op expression

--- src/typechecker/typechecker.ts
+++ src/typechecker/typechecker.ts
@@ -2,7 +2,7 @@ import * as es from 'estree'
 
 import { ErrorSeverity, ErrorType, Node, SourceError } from '../types'
 
-type Table = { [key: string]: string }
+type TypeEnvironment = { [key: string]: string }
 
 export class FatalTypeError implements SourceError {
   public type = ErrorType.SYNTAX
@@ -18,7 +18,36 @@ export class FatalTypeError implements SourceError {
   }
 }
 
-function check(node: Node | undefined, S: Table, F: Table): string | undefined {
+function checkSymType(
+  sym: string,
+  leftExprType: string | undefined,
+  rightExprType: string | undefined
+): string | undefined {
+  switch (sym) {
+    case '+':
+    case '-':
+      if (leftExprType != 'int' || rightExprType != 'int') {
+        throw new FatalTypeError(
+          {
+            start: {
+              line: 0,
+              column: 0
+            },
+            end: {
+              line: 0,
+              column: 0
+            }
+          },
+          `Binary op must be int int: instead found ${leftExprType} and ${rightExprType}`
+        )
+      }
+      return 'int'
+    default:
+      return undefined
+  }
+}
+
+function check(node: Node | undefined, E: TypeEnvironment): string | undefined {
   if (!node) {
     return
   }
@@ -28,19 +57,19 @@ function check(node: Node | undefined, S: Table, F: Table): string | undefined {
   switch (tag) {
     case 'CompilationUnit':
       const { translationUnit } = node
-      return check(translationUnit, S, F)
+      return check(translationUnit, E)
 
     case 'TranslationUnit':
       const { externalDeclarations } = node
-      return externalDeclarations.reduce((_acc, curr) => check(curr, S, F), undefined)
+      return externalDeclarations.reduce((_acc, curr) => check(curr, E), undefined)
 
     case 'ExternalDeclaration':
       const { functionDefinition, declaration } = node
-      return check(functionDefinition, S, F) || check(declaration, S, F)
+      return check(functionDefinition, E) || check(declaration, E)
 
     case 'Declaration':
       const { type, identifier, initializer } = node
-      const initializerType = check(initializer, S, F) || type
+      const initializerType = check(initializer, E) || type
       if (type != initializerType) {
         throw new FatalTypeError(
           {
@@ -56,15 +85,65 @@ function check(node: Node | undefined, S: Table, F: Table): string | undefined {
           `Type mismatch: ${initializerType} assigned to ${type} ${identifier}`
         )
       }
-      return (S[identifier] = type)
+      return (E[identifier] = type)
 
     case 'Initializer':
       const { expr: assignmentExpression } = node
-      return check(assignmentExpression, S, F)
+      return check(assignmentExpression, E)
 
     case 'AssignmentExpression':
       const { expr } = node
-      return check(expr, S, F)
+      return check(expr, E)
+
+    case 'ConditionalExpression':
+      const { pred, cons, alt } = node
+      const predType = check(pred, E)
+      if (predType != 'int') {
+        throw new FatalTypeError(
+          {
+            start: {
+              line: 0,
+              column: 0
+            },
+            end: {
+              line: 0,
+              column: 0
+            }
+          },
+          `Conditional expression predicate must be int: instead found ${predType}`
+        )
+      }
+
+      const consType = check(cons, E)
+      const altType = check(alt, E)
+      if (consType != altType) {
+        if (predType != 'int') {
+          throw new FatalTypeError(
+            {
+              start: {
+                line: 0,
+                column: 0
+              },
+              end: {
+                line: 0,
+                column: 0
+              }
+            },
+            `Conditional expression return types must be the same: instead found ${cons} and ${alt}`
+          )
+        }
+      }
+      return check(expr, E)
+
+    case 'ExpressionList':
+      const { exprs } = node
+      return exprs.reduce((_acc, curr) => check(curr, E), undefined)
+
+    case 'BinaryOpExpression':
+      const { sym, leftExpr, rightExpr } = node
+      const leftExprType = check(leftExpr, E)
+      const rightExprType = check(rightExpr, E)
+      return checkSymType(sym, leftExprType, rightExprType)
 
     case 'Number':
       return 'int'
@@ -93,7 +172,6 @@ function check(node: Node | undefined, S: Table, F: Table): string | undefined {
 }
 
 export function checkTyping(program: Node) {
-  const S: Table = {}
-  const F: Table = {}
-  check(program, S, F)
+  const E: TypeEnvironment = {}
+  check(program, E)
 }
